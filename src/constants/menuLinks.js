e712dbb81173af2ea559770a0098ac625907123f
flwlss <dimaandreev1337@gmail.com>
2021-07-17 14:19:59 +0700

    first commit

---
+++ src/constants/menuLinks.js
@@ -0,0 +1,7 @@
+export const menuList = [
+    { name: "Home", path: "/" },
+    { name: "About Us", path: "/about" },
+    { name: "Property", path: "/property" },
+    { name: "Our blog", path: "/blog" },
+    { name: "Contacts", path: "/contacts" },
+]
