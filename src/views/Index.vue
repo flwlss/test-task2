29a625c5421f0383065985bc3dcc72153329e56f
flwlss <dimaandreev1337@gmail.com>
2021-07-18 23:00:38 +0700

    add index

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="index">
-    <h1>INDEX</h1>
+    <div class="index-circle">
+      <img alt="index-logo" src="../assets/mail.svg" />
+    </div>
+    <p class="index-title">Без регистрации</p>
+    <p class="index-text">Введите адрес электронной почты</p>
+    <input class="index-input" type="email" placeholder="Введите email" />
+    <a class="index-continue" href="">Продолжить</a>
+    <a class="index-login" href="#">Вход через Госуслуги</a>
+    <!-- сМЕНИТЬ ШРИФТ -->
+    <!-- сМЕНИТЬ ШРИФТ -->
+    <!-- сМЕНИТЬ ШРИФТ -->
+    <!-- сМЕНИТЬ ШРИФТ -->
   </div>
 </template>
